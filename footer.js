49f7aceb0c28ef6f45c7ab6ccdc6412ef60e9c58
sajakshrestha8 <shresthasajak123@gmail.com>
2024-02-23 09:43:05 +0545

    file name changed

--- footer.js
+++ footer.js
@@ -4,30 +4,42 @@ class footer extends HTMLElement{
         <footer>
 
         <div class="col1">
-            <img src="images/logo.png" alt="logo" width="150px" height="150px">
-            <h3>CONTACT</h3>
-            <p><strong>Address: </strong> Putalisadak, Kathmandu</p>
-            <p><strong>Phone number: </strong> Putalisadak, Kathmandu</p>
-            <p><strong>Feedback: </strong>nomailtome@gmail.com</p>
+        <label class="maphead"><b>VIEW OUR LOCATION</b></label>
+        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d7065.175083057745!2d85.2851742305553!3d27.699140141706298!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x39eb199bd33966a7%3A0x42fe38c4499c44e0!2sRed%20Cross%20International!5e0!3m2!1sen!2snp!4v1708582347752!5m2!1sen!2snp" width="500" height="350" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
         </div>
 
         <div class="col2">
-            <h3>ABOUT US</h3>
-            <a href="#">About</a>
-            <a href="#">Delivery Information</a>
-            <a href="#">Privacy Policy</a>
-            <a href="#">Terms and Conditions</a>
-            <a href="#">Contact us</a>
+            <label class="maphead1"><b>CONTACT US</b></label>
+            <div class="info1">
+            <div class="contactinfo1">
+                <label class="contacttitle1"><b>Address</b></label><br>
+                <label class="contactbody1">Red Cross Marga, Kalimati</label><br><br>
+                <label class="contacttitle1"><b>Email</b></label><br>
+                <label class="contactbody1">nrcs@nrcs.org.info@nrcs.org</label><br><br>
+                <label class="contacttitle1"><b>Phone</b></label><br>
+                <label class="contactbody1">+977-1-5370650, +977-1-537 2761</label><br><br>
+                <label class="contacttitle1"><b>Post Box</b></label><br>
+                <label class="contactbody1">Post Box No.217</label><br><br>
+                <label class="contacttitle1"><b>Fax</b></label><br>
+                <label class="contactbody1">+977-4271915, 4273285</label><br>
+            </div>
+        </div>
         </div>
 
         <div class="col-install">
-            <label>View Our Location</label>
-            <p>From the App Store or Google play</p>
-            <div class="row">
+            <label class="maphead2"><b>SOCIALIZE WITH US</b></label><br>
+            <div class="image-container">
+                <a href="https://www.facebook.com/nepalredcross" target="_blank"><img src="Image/Facebook.png" alt="facebook" class="icon-logo">
             </div>
-            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d7065.175083057745!2d85.2851742305553!3d27.699140141706298!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x39eb199bd33966a7%3A0x42fe38c4499c44e0!2sRed%20Cross%20International!5e0!3m2!1sen!2snp!4v1708582347752!5m2!1sen!2snp" width="500" height="350" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
-            <p>Secured Payment Gateways</p>
-        </div>
+            <div class="image-container">
+                <a href="https://www.instagram.com/americanredcross/" target="_blank"><img src="Image/instagram.png" alt="facebook" class="icon-logo">
+            </div>
+            <div class="image-container">
+                <a href="https://twitter.com/NepalRedCross" target="_blank"><img src="Image/pngegg.png" alt="facebook" class="icon-logo1">
+            </div>
+            <div class="image-container">
+                <a href="https://www.youtube.com/channel/UCwA1CYDYvIH7KaRBpYy50ow" target="_blank"><img src="Image/youtube.png" alt="facebook" class="icon-logo1">
+            </div>
       </footer>
       <div class="footer-bottom">
         <p>&copy; 2023 Your Website. All rights reserved.</p>
--- try.js
+++ try.js
@@ -3,10 +3,11 @@ class sajak extends HTMLElement{
         this.innerHTML = `<header>
         <nav class="hover" id="SearchForm" onsubmit="return validateSearch()">
             <ul>
-              <img src="image/logo.jpg" alt="logo" height="150" class="image">
+              <img src="image/logo.png" alt="logo" height="150" class="image">
                 <li><a href="home.html"><b>Home</b></a></li>
                 <li><a href="blood.html"><b>Give Blood</b></a></li>
                 <li><a href="News.html"><b>News and Updates</b></a></li>
+                <li><a href="Training.html"><b>Training</b></a></li>
                 <li><a href="Member.html"><b>Get involved?</b></a></li>
                 <li><a href="About.html"><b>About Us</b></a></li>
                 <li><a href="Contact.html"><b>Contact Us</b></a></li>
@@ -22,20 +23,20 @@ class sajak extends HTMLElement{
 customElements.define('my-header',sajak)
 
 
-// When the user scrolls the page, execute myFunction
-window.onscroll = function() {myFunction()};
+// // When the user scrolls the page, execute myFunction
+// window.onscroll = function() {myFunction()};
 
-// Get the navbar
-var navbar = document.getElementById("SearchForm");
+// // Get the navbar
+// var navbar = document.getElementById("SearchForm");
 
-// Get the offset position of the navbar
-var sticky = navbar.offsetTop;
+// // Get the offset position of the navbar
+// var sticky = navbar.offsetTop;
 
-// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
-function myFunction() {
-if (window.pageYOffset >= sticky) {
-navbar.classList.add("sticky")
-} else {
-navbar.classList.remove("sticky");
-}
-}
+// // Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
+// function myFunction() {
+// if (window.pageYOffset >= sticky) {
+// navbar.classList.add("sticky")
+// } else {
+// navbar.classList.remove("sticky");
+// }
+// }
